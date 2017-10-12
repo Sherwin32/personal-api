a206153313a26a26bdedbff80309b8bde0ba4c73
Sherwin32 <sherwin.shann@gmail.com>
2017-10-12 13:43:34 -0700

    hardcoded profile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,46 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+	var $display = $('#display');
+
+
+
+
+
+	$('.profile').on('click', function(e){
+		e.preventDefault();
+
+		$.ajax({
+        method: "GET",
+        url: '/api/profile',
+        success: function onIndexSuccess(json) {
+            console.log(json);
+            $display.text("");
+           	// $display.append(JSON.stringify(json.data));
+
+            $display.append(`
+            	<h2>My Profile</h2>
+              <ul>
+                <li>Name: ${json.data.name}</li>
+                <li>Github: <a href="${json.data.githubLink}">Click me</a></li>
+                <li>Personal Site: <a href="${json.data.personalSiteLink}">Click me</a></li>
+                <li>Current City: ${json.data.currentCity}</li>
+              </ul>
+            `);
+            if(json.data.pets){
+                $display.append(`<h3>Pets</h3>`);
+            	var petsArray = json.data.pets;
+            	petsArray.forEach(function(pet){
+            		$display.append(`<h4>${pet.name}</h4>`)
+            		$display.append(`<ul><li>type: ${pet.type}</li>breed: ${pet.breed}<li>birthday: ${pet.birthday}</li></ul>`)
+            	})
+            }
+
+        },
+        error: function onError(a, b, c) {
+            console.log(a, b, c);
+        }
+    });
+	})
 
 });
--- server.js
+++ server.js
@@ -21,6 +21,28 @@ app.use(function(req, res, next) {
 
 // var db = require('./models');
 
+var profile = {
+  name: "Sherwin Shann",
+  githubLink: "https://github.com/Sherwin32",
+  githubProfileImage: "https://avatars1.githubusercontent.com/u/25252453?v=4&s=400&u=68c915801f66fdb382b0850fbbe1d703ebaa6c63",
+  personalSiteLink: "https://sherwin32.github.io/",
+  currentCity: "San Francisco",
+  pets:[
+    {
+      name: "Shapy",
+      type: "Dog",
+      breed: "Corgy",
+      birthday: "12/28/2009"
+    },
+    {
+      name: "Dwyane",
+      type: "Cat",
+      breed: "Domestic short hair",
+      birthday: "05/21/2014"
+    }
+  ]
+}
+
 /**********
  * ROUTES *
  **********/
@@ -46,16 +68,22 @@ app.get('/api', function apiIndex(req, res) {
   // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
   // It would be seriously overkill to save any of this to your database.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    woopsIForgotToDocumentAllMyEndpoints: false, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
     documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
     baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "A brief description about me"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
     ]
   })
+});
+
+app.get('/api/profile', function apiIndex(req, res) {
+  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
+  // It would be seriously overkill to save any of this to your database.
+  res.json({'data':profile});
 });
 
 /**********
